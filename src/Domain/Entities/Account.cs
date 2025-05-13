e6c703a7982aa96f1dfb9c82376ddcb21a4b01e3
Lucas Padilha <lucas_padilha@outlook.com>
2025-05-13 00:30:13 -0300

    create domain layer and Account entity

---
+++ src/Domain/Entities/Account.cs
@@ -0,0 +1,24 @@
+﻿namespace src.Domain.Entities
+{
+    public class Account
+    {
+        public string Id { get; private set; }
+        public decimal Balance { get; private set; }
+
+        public Account(string id)
+        {
+            Id = id;
+            Balance = 0;
+        }
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        public Boolean Withdraw(decimal amount)
+        {
+            if (Balance < amount) return false;
+            Balance -= amount;
+            return true;
+        }
+    }
+}
