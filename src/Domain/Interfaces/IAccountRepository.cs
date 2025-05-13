8399c344028a6bd0758c7d1ae4320c89486742ab
Lucas Padilha <lucas_padilha@outlook.com>
2025-05-13 00:31:41 -0300

    create IAccountRepository in Domain layer

---
+++ src/Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using src.Domain.Entities;
+
+namespace src.Domain.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Account GetById(string id);
+        void Create(Account account);
+        void Update(Account account);
+        decimal Balance(Account account);
+        void Reset();
+    }
+}
